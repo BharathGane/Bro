eb1d83eacc5278f6402ad06d131166d522f34fe5
adityathiru <T.Aditya@iiitb.org>
2016-03-09 15:42:20 +0530

    Updated LMS script

--- Docs/LMS.py
+++ Docs/LMS.py
@@ -1,6 +1,7 @@
 import re
 import requests
 import selenium
+from lxml import html
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,7 +9,7 @@ from selenium.webdriver.support.ui import Select
 
 s = requests.session()
 
-payload={'username':'IMTXXXXXXX', 'password':'XXXXXXXX'}
+payload={'username':'IMTXXXXXXX', 'password':'XXXXXXXXXXXX'}
 
 def login():
     url = s.post('https://lms.iiitb.ac.in/moodle/login/index.php', data=payload, verify=False)
@@ -21,29 +22,45 @@ def login():
     else:
         print "Not logged in"
         return 0;
+def individual_details(link):
+    url = s.get(link)
+    soup = BeautifulSoup(url.text)
+    details = soup.find_all("div", class_="activityinstance")
+    tree = html.fromstring(url.content)
+    data = tree.xpath('//span[@class="instancename"]/text()')
+    j=0
+    for i in details:
+        link = i.findChildren()[0]['href']
+        if(len(details)>=1):
+            print "\t" + data[j]
+            print "\t" + "Link :" + link
+            j+=1
+    print "\n"
 
 def check_new_assignments():
-    url = s.post('https://lms.iiitb.ac.in/moodle/my/', data=payload, verify=False)
+    url = s.get('https://lms.iiitb.ac.in/moodle/my/')
     soup = BeautifulSoup(url.text)
-    t = soup.findAll(text=re.compile("You have assignments that need attention"))
+    check_assignments = soup.findAll(text=re.compile("You have assignments that need attention"))
 
-    for i in t:
+    for i in check_assignments:
         link = i.parent.parent.parent.parent.parent.parent.findChild().findChild().findChildren()[0]['href']
         subject = i.parent.parent.parent.parent.parent.parent.findChild().findChild().findChildren()[0]['title']
 
-        if(len(t)>=1):
+
+        if(len(check_assignments)>=1):
             print i + "in "+ subject
-            print "Link: " + link + "\n"
+            print "Link: " + link
+            individual_details(link)
 
 def check_new_forum_posts():
-    url = s.post('https://lms.iiitb.ac.in/moodle/my/', data=payload, verify=False)
+    url = s.get('https://lms.iiitb.ac.in/moodle/my/')
     soup = BeautifulSoup(url.text)
-    t = soup.findAll(text=re.compile("There are new forum posts"))
+    check_posts = soup.findAll(text=re.compile("There are new forum posts"))
 
-    for i in t:
+    for i in check_posts:
         link = i.parent.parent.parent.parent.parent.parent.findChild().findChild().findChildren()[0]['href']
-        subject = t[0].parent.parent.parent.parent.parent.parent.findChild().findChild().findChildren()[0]['title']
-        if(len(t)>=1):
+        subject = check_posts[0].parent.parent.parent.parent.parent.parent.findChild().findChild().findChildren()[0]['title']
+        if(len(check_posts)>=1):
             print i + "in " + subject
             print "Link: " + link + "\n"
 
