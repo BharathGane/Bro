7663f821104a36a48e7a59f31bde5b86795cb36b
adityathiru <T.Aditya@iiitb.org>
2016-03-10 13:31:45 +0530

    Added dependency install script

---
+++ INSTALL.sh
@@ -0,0 +1,8 @@
+echo 'This script will install the required dependencies required to run Bro'
+echo 'Please enter your password: '
+sudo pip install simplejson
+sudo pip install apiai
+sudo pip install selenium
+sudo apt-get install alsa-utils
+sudo apt-get install xbacklight
+sudo apt-get install python-pyaudio python-numpy
